be8ba918f15d9c7329fd02a572ba6b6aa4850e29
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-10-01 11:33:52 +0200

    Setup front-side authentification : route

---
+++ resources/js/router.js
@@ -0,0 +1,56 @@
+import VueRouter from 'vue-router'
+
+import Home from './components/Home'
+import Register from './components/Register'
+import Login from './components/Login'
+import Dashboard from './components/user/Dashboard'
+import AdminDashboard from './components/admin/Dashboard'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            auth: undefined
+        }
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        meta: {
+            auth: false
+        }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            auth: false
+        }
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: {
+            auth: true
+        }
+    },
+    {
+        path: '/admin',
+        name: 'admin.dashboard',
+        component: AdminDashboard,
+        meta: {
+            auth: {roles: 2, redirect: {name: 'login'}, forbiddenRedirect: '/403'}
+        }
+    },
+]
+
+const router = new VueRouter({
+    history: true,
+    mode: 'history',
+    routes,
+})
