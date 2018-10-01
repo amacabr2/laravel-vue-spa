19826774e2e3c183e3aa0459194efc68ca76ea25
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-10-01 10:42:33 +0200

    API route protection with JWT

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
+
+    public function handle($request, Closure $next, ...$guards) {
+        if ($this->authenticate($request, $guards) === 'authentification_error') {
+            return response()->json(['error' => 'Unauthorized']);
+        }
+
+        return $next($request);
+    }
+
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
+     * @param \Illuminate\Http\Request $request
+     * @param array $guards
+     * @return string|void
      */
-    protected function redirectTo($request)
-    {
-        return route('login');
+    protected function authenticate($request, array $guards) {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        return 'authentification_error';
     }
 }
